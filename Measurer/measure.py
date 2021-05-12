aa47063e482336feb8f356646161e5647c139efb
Lorenzo De Milleri <36283064+ldemilleri@users.noreply.github.com>
2021-05-12 14:15:34 +0200

    Project Uploaded
    Full project uploaded from local

---
+++ Measurer/measure.py
@@ -0,0 +1,42 @@
+import cv2
+import util
+
+#Constants
+webcam = True
+path = "test_image.jpg"
+cap = cv2.VideoCapture(0)
+cap.set(10,160)
+cap.set(3,1920)
+cap.set(4,1080)
+scale = 3
+wP = 210 *scale
+hP= 297 *scale
+
+while True:
+    if webcam:success,img = cap.read()
+    else: img = cv2.imread(path)
+
+    imgContours , conts = util.getContours(img,minArea=50000,filter=4)
+    if len(conts) != 0:
+        biggest = conts[0][2]
+        #print(biggest)
+        imgWarp = util.warpImg(img, biggest, wP,hP)
+        imgContours, conts = util.getContours( imgWarp, minArea=2000, filter=4, cThr=[50,50], draw = False)
+        if len(conts) != 0:
+            for obj in conts:
+                cv2.polylines(imgContours,[obj[2]],True,(0,255,0),2)
+                nPoints = util.reorder(obj[2])
+                nW = round((util.findDis(nPoints[0][0]//scale,nPoints[1][0]//scale)/10),1)
+                nH = round((util.findDis(nPoints[0][0]//scale,nPoints[2][0]//scale)/10),1)
+                cv2.arrowedLine(imgContours, (nPoints[0][0][0], nPoints[0][0][1]), (nPoints[1][0][0], nPoints[1][0][1]),
+                                (255, 0, 255), 3, 8, 0, 0.05)
+                cv2.arrowedLine(imgContours, (nPoints[0][0][0], nPoints[0][0][1]), (nPoints[2][0][0], nPoints[2][0][1]),
+                                (255, 0, 255), 3, 8, 0, 0.05)
+                x, y, w, h = obj[3]
+                cv2.putText(imgContours, "{}cm".format(nW), (x + 30, y - 10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5, (255, 0, 255), 2)
+                cv2.putText(imgContours, "{}cm".format(nH), (x - 70, y + h // 2), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5, (255, 0, 255), 2)
+        cv2.imshow("A4", imgContours)
+
+    img = cv2.resize(img,(0,0),None,0.5,0.5)
+    cv2.imshow("Original",img)
+    cv2.waitKey(1)
